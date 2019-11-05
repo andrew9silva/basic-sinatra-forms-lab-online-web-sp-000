19379c5d8bd046d201bf54a06e7d6560ed0eb6f0
Andrew Silva <andrewsilva9@gmail.com>
2019-11-05 02:59:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   
   post "/team" do
     @name = params["name']
-    @coach = params["coach']
+    @coach = params["coach"]
     @pg = params["pg']
     @sg = params["sg']
     @sf = params["sf']
